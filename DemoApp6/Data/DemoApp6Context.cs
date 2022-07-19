0d56589a6058f36826ee3fadcc36bdabb4bcb88a
harik <harik@DESKTOP-6PB412V>
2022-07-19 16:34:41 +0300

    Add project files.

---
+++ DemoApp6/Data/DemoApp6Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoApp6.Models;
+
+namespace DemoApp6.Data
+{
+    public class DemoApp6Context : DbContext
+    {
+        public DemoApp6Context (DbContextOptions<DemoApp6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
---
+++ DemoApp6/Models/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoApp6.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        
+        [Column(TypeName = "decima(18,2)")]
+        public decimal Price { get; set; }
+        //public string Rating { get; set; }
+    }
+}
